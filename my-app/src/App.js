ad35bda14e10eaa2e2b2cbac15651aa2fbbd2444
Luke Zhuang <lukezhuang@Lukes-Laptop-7.lan>
2025-04-24 15:50:46 -0700

    added youtube logo and hamburger menu

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,6 @@
 import { MdHome, MdSubscriptions, MdMusicNote, MdOutlineVideoLibrary } from "react-icons/md";
+import logo from './images/logo.png';
+import menu from './images/menu.png';
 import './App.css';
 
 
@@ -6,10 +8,14 @@ function App() {
   return (
     <div className="HomePage">
       <aside className="LeftSideBar">
-        <button className="LeftSideBarButton"><MdHome className="IconStyle" /> Home</button>
-        <button className="LeftSideBarButton"><MdOutlineVideoLibrary className="IconStyle" /> Shorts</button>
-        <button className="LeftSideBarButton"><MdSubscriptions className="IconStyle" /> Subscriptions</button>
-        <button className="LeftSideBarButton"><MdMusicNote className="IconStyle" /> Youtube Music</button>
+        <div className="TopBar">
+          <img src={menu} alt="Youtube Hamburger Menu" className = "YoutubeMenu"></img>
+          <img src={logo} alt="YouTube Logo" className="YouTubeLogo" />
+        </div>
+          <button className="LeftSideBarButton"><MdHome className="IconStyle" /> Home</button>
+          <button className="LeftSideBarButton"><MdOutlineVideoLibrary className="IconStyle" /> Shorts</button>
+          <button className="LeftSideBarButton"><MdSubscriptions className="IconStyle" /> Subscriptions</button>
+          <button className="LeftSideBarButton"><MdMusicNote className="IconStyle" /> Youtube Music</button>
       </aside>
     </div>
   );
