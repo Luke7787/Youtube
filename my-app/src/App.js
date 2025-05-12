5dafb869cfb9443a5e36683a8798999d44bebf03
Luke Zhuang <lukezhuang@Lukes-Laptop-7.local>
2025-05-12 16:17:16 -0700

    added search bar icon in search bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -893,11 +893,24 @@ function App() {
 
       <main className="MainContent">
         <div className="SearchContainer">
-        <input 
-          type="text" 
-          className="SearchBar" 
-          placeholder="Search" 
-        />
+          <input 
+            type="text" 
+            className="SearchBar" 
+            placeholder="Search" 
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="currentColor" 
+            height="24" 
+            viewBox="0 0 24 24" 
+            width="24" 
+            className="SearchIcon" 
+          >
+            <path clip-rule="evenodd" 
+              d="M16.296 16.996a8 8 0 11.707-.708l3.909 3.91-.707.707-3.909-3.909zM18 11a7 7 0 00-14 0 7 7 0 1014 0z" 
+              fill-rule="evenodd"
+            />
+          </svg>
         </div>
       </main>
     </div>
